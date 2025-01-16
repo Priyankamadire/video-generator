ff2355dfb7470ee03ccc0c9c3b60dddd49b6c475
Madire Priyanka <108828579+Priyankamadire@users.noreply.github.com>
2025-01-16 18:20:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import argparse
 import asyncio
 import requests
 from bs4 import BeautifulSoup
-from script_generator import generate_script
+from utility.script.script_generator import generate_script
 from utility.audio.audio_generator import generate_audio
 from utility.captions.timed_captions_generator import generate_timed_captions
 from utility.render.render_engine import render_video
